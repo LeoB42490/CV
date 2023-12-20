601db67ee2f54dabfd848114581a0fb5a11a7d7b
Borie <leoborie4@gmail.com>
2023-12-20 12:51:46 +0100

    Dernier essai
    Ancien CV 19/12 -> Responsive avec Bootstrap
    Nouveau CV 20/12 -> Responsive

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
+// Script permettant d'ouvrir les boutons.
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    } 
+  });
+}
+
+// Script permettant de changer la couleur des boutons
 function RandomColor(){
-    let  accordionHeaders = document.getElementsByClassName("accordion-header");
+  let  collapsible = document.querySelectorAll("div.Tout > button");
 
-        console.log(accordionHeaders.length)
-    for (let i = 0; i < accordionHeaders.length; i++) {
-        let randomRed = Math.floor(Math.random() * 255);
-        let randomGreen = Math.floor(Math.random() * 255);
-        let randomBlue = Math.floor(Math.random() * 255);
+      console.log(collapsible.length)
+  for (let i = 0; i < collapsible.length; i++) {
+      let randomRed = Math.floor(Math.random() * 255);
+      let randomGreen = Math.floor(Math.random() * 255);
+      let randomBlue = Math.floor(Math.random() * 255);
 
-        //accordionHeaders[i].innerHTML = "<a href=\"x.com\">tweeter</a>";
-        accordionHeaders[i].firstElementChild.style.color = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`
-    }
+      //accordionHeaders[i].innerHTML = "<a href=\"x.com\">tweeter</a>";
+      collapsible[i].style.color = `rgb(${randomRed}, ${randomGreen}, ${randomBlue})`
+  }
 }
 
-// addEventListener("click", RandomColor)
+//addEventListener("click", RandomColor)
 setInterval(RandomColor, 600)
-
-/* 
-<script type="text/javascript">
-var isDark = false;
-function switchTheme() {
-    let color = isDark ? "white" : "black";
-    document.getElementsByTagName("html").item(0).style.backgroundColor = color;
-    document.getElementById("navbar-example2").style.color = color;
-    isDark = !isDark;
-}
-</script>
-*/
